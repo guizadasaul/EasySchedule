c64c51f9d89b724aa73a6648014a0d90e64f4d43
LokoAddams <lucasalcoba74@gmail.com>
2026-03-17 04:17:11 -0400

    Removes obsolete edit profile component and routing

--- frontend/src/app/features/perfil/perfil.routes.ts
+++ frontend/src/app/features/perfil/perfil.routes.ts
@@ -2,10 +2,5 @@ import { Routes } from '@angular/router';
 import { Perfil } from './perfil';
 
 export const PERFIL_ROUTES: Routes = [
-  { path: '', component: Perfil },
-  {
-    path: 'editar',
-    loadComponent: () =>
-      import('./editar-perfil/editar-perfil').then((m) => m.EditarPerfil),
-  },
+  { path: '', component: Perfil }
 ];
