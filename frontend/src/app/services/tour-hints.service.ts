1860a6a046a2b6a657d46efb7fc29104c905dbd5
Adrian Claros <166774927+AdrianCV2345@users.noreply.github.com>
2026-05-05 23:34:42 -0400

    fix: frontend test fixed.

--- frontend/src/app/services/tour-hints.service.ts
+++ frontend/src/app/services/tour-hints.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Subject } from 'rxjs';
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class TourHintsService {
-  // Observable para controlar si el popover de "Toma de Materias" debe estar abierto
-  private tomaMateriasPopoverOpen$ = new BehaviorSubject<boolean>(false);
-  tomaMateriasPopoverOpen = this.tomaMateriasPopoverOpen$.asObservable();
+  private tomaMateriasPopover?: NgbPopover;
+
+  private tomaMateriasPopoverOpenSubject = new Subject<boolean>();
+
+  public tomaMateriasPopoverOpen = this.tomaMateriasPopoverOpenSubject.asObservable();
+
+  setTomaMateriasPopover(popover: NgbPopover): void {
+    this.tomaMateriasPopover = popover;
+  }
 
   openTomaMateriasPopover(): void {
-    this.tomaMateriasPopoverOpen$.next(true);
+    this.tomaMateriasPopover?.open();
+    this.tomaMateriasPopoverOpenSubject.next(true);
   }
 
   closeTomaMateriasPopover(): void {
-    this.tomaMateriasPopoverOpen$.next(false);
+    this.tomaMateriasPopover?.close();
+    this.tomaMateriasPopoverOpenSubject.next(false);
+  }
+
+  requestOpenTomaMateriasPopover(): void {
+    this.tomaMateriasPopoverOpenSubject.next(true);
+  }
+
+  requestCloseTomaMateriasPopover(): void {
+    this.tomaMateriasPopoverOpenSubject.next(false);
   }
 }
